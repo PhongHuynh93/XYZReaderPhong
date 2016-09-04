21182ab8652744607248a5f93fdafe62c2e41f00
Phong <p7157568@gmail.com>
2016-09-04 15:41:01 +0700

    feat - add layout and bind to code java

--- app/src/main/java/dhbk/android/xyzreaderphong/view/impl/ArticleListActivity.java
+++ app/src/main/java/dhbk/android/xyzreaderphong/view/impl/ArticleListActivity.java
@@ -2,20 +2,31 @@ package dhbk.android.xyzreaderphong.view.impl;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dhbk.android.xyzreaderphong.R;
-import dhbk.android.xyzreaderphong.view.ArticleListView;
-import dhbk.android.xyzreaderphong.presenter.loader.PresenterFactory;
-import dhbk.android.xyzreaderphong.presenter.ArticleListPresenter;
 import dhbk.android.xyzreaderphong.injection.AppComponent;
 import dhbk.android.xyzreaderphong.injection.ArticleListViewModule;
 import dhbk.android.xyzreaderphong.injection.DaggerArticleListViewComponent;
-
-import javax.inject.Inject;
+import dhbk.android.xyzreaderphong.presenter.ArticleListPresenter;
+import dhbk.android.xyzreaderphong.presenter.loader.PresenterFactory;
+import dhbk.android.xyzreaderphong.view.ArticleListView;
 
 public final class ArticleListActivity extends BaseActivity<ArticleListPresenter, ArticleListView> implements ArticleListView {
     @Inject
     PresenterFactory<ArticleListPresenter> mPresenterFactory;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     // Your presenter is available using the mPresenter variable
 
@@ -23,10 +34,12 @@ public final class ArticleListActivity extends BaseActivity<ArticleListPresenter
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
+        ButterKnife.bind(this);
 
         // Your code here
         // Do not call mPresenter from here, it will be null! Wait for onStart or onPostCreate.
     }
+
 
     @Override
     protected void setupComponent(@NonNull AppComponent parentComponent) {
